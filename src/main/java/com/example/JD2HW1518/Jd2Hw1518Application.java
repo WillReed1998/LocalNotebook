a1c643c63911e1c37b869cb38b6aa006d6ae84d4
Nikita Kovalov <nik.kovalev1760@gmail.com>
2023-11-10 05:15:33 -0500

    Initial commit

---
+++ src/main/java/com/example/JD2HW1518/Jd2Hw1518Application.java
@@ -0,0 +1,13 @@
+package com.example.JD2HW1518;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jd2Hw1518Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jd2Hw1518Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/JD2HW1518/NoteService.java
@@ -0,0 +1,61 @@
+package com.example.JD2HW1518;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class NoteService {
+    private final List<Note> notes = new ArrayList<>();
+    private int idCounter = 0;
+    private long generatedId () {
+        return idCounter++;
+    };
+
+    List<Note> listAll() {
+        return new ArrayList<>(notes);
+    }
+
+    Note add(Note note) {
+        long id = generatedId();
+        note.setId(id);
+        notes.add(note);
+        return note;
+    }
+
+    void deleteById(long id) {
+        boolean removed = notes.removeIf(note -> note.getId() == id);
+        if (!removed) {
+            throw new NullPointerException("Id" + id +"already deleted");
+        }
+
+
+    }
+
+    void update(Note note) {
+        long id = note.getId();
+        boolean found = false;
+
+        for (Note existingNote : notes) {
+            if (existingNote.getId() == id) {
+                existingNote.setTitle(note.getTitle());
+                existingNote.setContent(note.getContent());
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            throw new NullPointerException("Id" + id + "not found and can't  be updated");
+        }
+    }
+
+    public Note getById(long id) {
+        for (Note note : notes) {
+            if (note.getId() == id) {
+                return note;
+            }
+        }
+        throw new NullPointerException("Id" + id + "not found");
+    }
+}
